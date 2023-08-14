82c6c9ae48b0ebd87539368a9e92b6084239a4d4
kdloosararian <134308829+kdloosararian@users.noreply.github.com>
2023-08-14 12:12:27 -0400

    Delete dept_employees.sql

--- EmployeeSQL/dept_employees.sql
+++
@@ -1,8 +0,0 @@
-Create Table Dept_Employees(
-emp_no INT,
-	dept_no VARCHAR (30),
-	
-	FOREIGN Key (emp_no) References Employees(emp_no),
-	FOREIGN Key (dept_no) References Departments(dept_no),
-	Primary key (emp_no, dept_no)
-);
