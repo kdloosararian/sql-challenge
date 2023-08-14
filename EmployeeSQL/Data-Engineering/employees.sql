95dded58b75e47c64dc38fb9eff69dcc877303cd
kdloosararian <134308829+kdloosararian@users.noreply.github.com>
2023-08-14 12:11:40 -0400

    Add files via upload

---
+++ EmployeeSQL/Data-Engineering/employees.sql
@@ -0,0 +1,14 @@
+Drop table Employees;
+
+CREATE TABLE Employees (
+    emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR (50),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex CHAR(1),
+    hire_date DATE
+);
+
+
+
---
+++ EmployeeSQL/Data-Engineering/titles.sql
@@ -0,0 +1,8 @@
+Drop table Titles;
+
+Create Table Titles(
+title_id VARCHAR (30),
+	title VARCHAR (50),
+	
+	Primary key (title_id, title)
+);
